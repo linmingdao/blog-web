b15ed13d7766ec60487d6f23288ba9f010d46b82
林明道 <linmingdao@onemt.com.cn>
2020-07-15 14:21:07 +0800

    update：更新模板

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import { authorize } from './authority/index';
 export default [
     {
         path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
         component: () => import('@app/modules/home/index.vue'),
         beforeEnter: (to, from, next) => authorize(to, from, next)
     }
